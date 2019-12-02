be0ddb2f50bce0f1ae1abbebef273310e3e516d3
Rogelio Alvarado Vilchis <rogalvil@gmail.com>
2019-12-02 09:39:11 -0600

    Get all repos

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,13 +1,25 @@
 class ReposController < ApplicationController
   def index
     @repos = @user.rels[:repos].get.data
+
+    template = @user.rels[:repos].get
+    @repos = get_repos(template)
   end
 
   def show
     @repo = @client.repo "rogalvil/#{params[:id]}"
     @rels = @repo.rels
     @branches = @repo.rels[:branches].get.data
-    #@merges = @repo.rels[:merges].get.data
-    @commits = @repo.rels[:commits].get.data
+    @repos = @repo.rels[:repos].get.data
+  end
+
+  private
+
+  def get_repos(template)
+    next_rel = template.rels[:next]
+    unless next_rel.nil?
+      repos = get_repos(next_rel.get)
+    end
+    return template.data + repos.to_a
   end
 end
